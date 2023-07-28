26ce13846231659508f239c19fda4df0e0b5b6ec
Khushi Soni <khushi@gmail.com>
2023-07-28 16:45:35 +0530

    Rspec2

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -12,7 +12,44 @@ RSpec.describe "Posts", type: :request do
       expect(response).to render_template(partial: '_basic')
     end
   end
+
+
+  describe "GET #new" do
+    it "assigns a new post to @post" do
+      get new_post_url
+      expect(assigns(:post)).to be_a_new(Post)
+    end
+
+    it "renders the new template" do 
+      get new_post_url
+      expect(response).to render_template(:new)
+    end
+  end
+
+
+  # describe "GET /posts/:id" do
+  #   it "displays a particular post" do
+  #     post = Post.create
+  #     get :show, params: { id: post.id }
+  #     expect(assigns(:post)).to eq(post)
+  #     expect(response).to render_template(:show)
+  #   end
+  # end
+
+
+  # describe "GET /restaurants/:restaurant_id/items/:id" do
+  #   it "get item response" do
+  #     user = User.create(name: "Aman", email: "aman@gmail.com", role: "basic_account", password: "abc@123")
+  #     restaurant = Restaurant.create(name: "Gurukripa", description: "Pure veg", user_id: user.id)
+  #     item = Item.create(name: "Samosa", price: '15', user_id: user.id, restaurant_id: restaurant.id)
+  #     get  restaurant_item_path(restaurant, item)
+  #     expect(response).to have_http_status(302)
+  #   end
+  # end
 end
+
+
+
 
 
 
