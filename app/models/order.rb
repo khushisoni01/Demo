c93bfe35714e359963d6631fbbbeb543b2585154
Khushi Soni <khushi@gmail.com>
2023-08-02 18:51:05 +0530

    Active Admin 1

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,11 @@ class Order < ApplicationRecord
 	belongs_to :account
 	belongs_to :product
 	validates :address, length: {in: 5..60}, presence: true
+
+
+	 def self.ransackable_attributes(auth_object = nil)
+       ["account_id", "address", "confirm", "created_at", "id", "price", "product_id", "updated_at"]
+     end
+
 end
+   
